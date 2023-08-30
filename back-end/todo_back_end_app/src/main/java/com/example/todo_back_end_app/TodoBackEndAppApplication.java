a965b9de71bab710de35bac68ea07431e63a8609
jongheonleee <qwefghnm1212@gmail.com>
2023-08-30 16:46:16 +0900

    setting

---
+++ back-end/todo_back_end_app/src/main/java/com/example/todo_back_end_app/TodoBackEndAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.todo_back_end_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoBackEndAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoBackEndAppApplication.class, args);
+	}
+
+}
---
+++ back-end/todo_back_end_app/src/test/java/com/example/todo_back_end_app/TodoBackEndAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todo_back_end_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBackEndAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
